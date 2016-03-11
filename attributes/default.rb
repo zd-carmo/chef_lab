9fefee8206da9b08f4c20a716b7d57b5c4c58189
soneill <soneill@zendesk.com>
2016-03-11 12:02:13 +0000

    ssh attris

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
-default[:ssh][:X11Forwarding] = "no" 
+default[:ssh][:X11Forwarding] = "no"
 default[:ssh][:listening_port] = 22
+default[:ssh][:allow_groups] = ["ssh-users"]
+default[:ssh][:permit_empty_password] = "no"
+default[:ssh][:allow_root_with_key] = true
+defautt[:ssh][:server_key_bits] = "1024"
