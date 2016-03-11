c40ee86ace47997523cd8cb3efbc5ac83454be3c
Daniel Carmo <vagrant@chefd.local>
2016-03-11 11:59:46 +0000

    .

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,4 +3,4 @@ default[:ssh][:listening_port] = 22
 default[:ssh][:allow_groups] = ["ssh-users"]
 default[:ssh][:permit_empty_password] = "no"
 default[:ssh][:allow_root_with_key] = true
-defautt[:ssh][:server_key_bits] = "1024"
+default[:ssh][:server_key_bits] = "1024"
